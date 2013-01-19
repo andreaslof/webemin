dc6681d709a13ac0c3377537253a005447351feb
Simon Westerlund <simon.westerlund@humblestorm.se>
2013-01-19 18:55:54 +0100

    added comments and copyright

--- webemin.js
+++ webemin.js
@@ -1,17 +1,8 @@
 /* webemin - a gyro-controlled theremin
  * © 2013 Andreas Hermansson, a@andreaslof.se
+ * © 2013 Simon Westerlund, s@simonwesterlund.se
  * */
 var webemin = {
-  options: {
-    LOWEST_FREQ:  120,
-    HIGHEST_FREQ: 1000
-  },
-  struct: {
-    SINE:     0,
-    SQUARE:   1,
-    SAW:      2,
-    TRIANGLE: 3
-  },
   frequency: 440, // default
   type: 0, // sine
   isPlaying: false,
@@ -20,13 +11,24 @@ var webemin = {
   _gainNode: null,
   _init: false,
   init: function() {
+    // create new context
     webemin._context = new webkitAudioContext();
+    // create new ocsillator
     webemin._ocsillator = webemin._context.createOscillator();
+    // create new gain node
+    // which is the volume controller (i think)
     webemin._gainNode = webemin._context.createGainNode();
+    // connect the ocsillator to the gain node
     webemin._ocsillator.connect(webemin._gainNode);
+    // set the default frequency to ocsillator
+    // in this case it is 440
     webemin._ocsillator.frequency.value = webemin.frequency;
+    // set the default type to sine
+    // available types: 0: sine, 1: square, 2: saw, 3: triangle
     webemin._ocsillator.type = 0;
+    // connect the gain node with the context
     webemin._gainNode.connect(webemin._context.destination);
+    // set volume to 0
     webemin._gainNode.gain.value = 0; // 0 volume
     webemin._init = true;
   },
