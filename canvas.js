8a143c7bf7a02da255ecfcfd551b66e325d394b8
Simon Westerlund <simon.westerlund@humblestorm.se>
2013-01-20 13:25:36 +0100

    New bend function which bends a bezier curve

--- canvas.js
+++ canvas.js
@@ -25,5 +25,29 @@ var canvas = {
     _.ctx.clearRect(0,0, _.width, _.height);
     _.ctx.fillStyle = fillStyle;
     _.ctx.fillRect(0, 0, _.width, _.height);
+  },
+  bend: function(freq){
+    var _ = canvas,
+        margin = 20;
+    _.ctx.clearRect(0,0, _.width, _.height);
+    
+    _.ctx.strokeStyle = 'rgba(0, 0, 0, 0.2)';
+    _.ctx.beginPath();
+    _.ctx.lineWidth = 10;
+    _.ctx.moveTo(margin, 100);
+    _.ctx.lineTo(_.width - (margin*2), 100);
+    _.ctx.stroke();
+
+    _.ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+    _.ctx.beginPath();
+    _.ctx.lineWidth = 2;
+    _.ctx.moveTo(margin, 100);
+    _.ctx.bezierCurveTo(100, // from left
+                        100,
+                        (_.width - (margin*2)) / 2, // total width / 2
+                        freq + 100, // depth
+                        _.width - (margin*2), // total width
+                        100); // from top
+    _.ctx.stroke();
   }
-}
+};
